3111e901483602e41f0431905def10fbd0943513
Nando354 <fernandoaguirre@gmail.com>
2023-05-30 19:22:52 -0700

    Added the input forms for expense tracker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import Expenses from "./components/Expenses/Expenses";
+import NewExpense from './components/NewExpense/NewExpense';
+
+import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
   const expenses = [
@@ -27,29 +29,8 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
-
-      {/* <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].afount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      /> */}
     </div>
   );
 }
